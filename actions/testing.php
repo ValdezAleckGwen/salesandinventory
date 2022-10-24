8baac06febdcb3af9b682f25e35b137183e9ccca
ValdezAleckGwen <aleckg.valdez@gmail.com>
2022-10-24 21:43:22 +0800

    created experiment

--- actions/testing.php
+++ actions/testing.php
@@ -3,15 +3,17 @@ include 'getdata.php';
 include 'database_connection.php';
 setlocale(LC_MONETARY, 'en_IN');
 
-
+  $output = '';
   $quantity = 1;
   $productprice = '₱18,150.75';
-  $productprice = filter_var($productprice, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+  $output .= "<li>".$productprice. "</li>";
+  $output .= "<li>".$productprice. "</li>";
+
+
+  echo $output;
   
-  $totalprice =  $quantity * $productprice;
-  // $formatotal = "₱" .number_format($totalprice);
-  $totalprice = number_format($totalprice, 2, '.', ',');
-  echo $totalprice;
+  
+ 
 
   
 
