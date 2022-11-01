37438ac5b024b2089c146e981e43546b74be0c9f
decreasing <kimjxsh@gmail.com>
2022-11-01 21:33:42 +0800

    fixed the responsiveness
    /stockmanager/dorder.php
    /stockmanager/purchaseorder.php
    /admin/stocktransfer.php
    /stockmanager/inventoryadjustment.php
    
    edited action files as well

--- actions/addrowinventoryadjustment.php
+++ actions/addrowinventoryadjustment.php
@@ -16,21 +16,21 @@ if (isset($_POST['branchid'])) {
 				$output = '';
 				$output .= '<tr style="display: block;">';
 
-				$output .= '<td  width="5%"><select name="item_id[]" class="col col-sm-2 form-control selectpicker item_id" data-live-search="true"><option value="">Select Product</option>';
+				$output .= '<td  width="13.9%"><select name="item_id[]" class="col col-sm-2 form-control selectpicker item_id" data-live-search="true"><option value="">Select Product</option>';
 				foreach ($inventories as $inventory) {
 				$output .= '<option value="'. $inventory['inventory'] .'">'. $inventory['inventory'] .'</option>';
 				}
 				$output .= '</select></td>';
 
-				$output .= '<td width="16%"><input type="text" name="item_code[]" class="col col-sm-2 form-control item_code" readonly/></td>';
+				$output .= '<td width="15.9%"><input type="text" name="item_code[]" class="col col-sm-2 form-control item_code" readonly/></td>';
 
 				$output .= '<td width="22.6%"><input type="text" name="item_name[]" class="col col-sm-5 form-control item_name" readonly/></td>';
 
-				$output .= '<td width="10%"><input type="number" name="item_quantity[]" class="col col-sm- form-control item_quantity" readonly/>';
+				$output .= '<td width="10%"><input type="number" name="item_quantity[]" class="col col-sm form-control item_quantity" readonly/>';
 
-				$output .= '<td width="16%"><input type="number" name="adjustment_quantityminus[]" class="col col-sm adjustment_quantityminus" ></td>';
+				$output .= '<td width="16%"><input type="number" name="adjustment_quantityminus[]" class="col col-sm form-control adjustment_quantityminus" ></td>';
 
-				$output .= '<td width="16%"><input type="number" name="adjustment_quantityplus[]" class="col col-sm adjustment_quantityplus" ></td>';
+				$output .= '<td width="16%"><input type="number" name="adjustment_quantityplus[]" class="col col-sm form-control adjustment_quantityplus" ></td>';
 
 
 
